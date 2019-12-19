f32d0dd7fecefef621e2b187381e83d327523572
Alex Gogl <gogl.alex@gmail.com>
2019-12-19 19:00:24 +0100

    update config to work with babel7

--- components/Logo.vue
+++ components/Logo.vue
@@ -7,9 +7,12 @@
   </div>
 </template>
 
+<script>
+export default {};
+</script>
+
 <style>
-.VueToNuxtLogo
-{
+.VueToNuxtLogo {
   display: inline-block;
   animation: turn 2s linear forwards 1s;
   transform: rotateX(180deg);
@@ -18,40 +21,35 @@
   height: 180px;
   width: 245px;
 }
-.Triangle
-{
+.Triangle {
   position: absolute;
   top: 0;
   left: 0;
   width: 0;
   height: 0;
 }
-.Triangle--one
-{
+.Triangle--one {
   border-left: 105px solid transparent;
   border-right: 105px solid transparent;
-  border-bottom: 180px solid #41B883;
+  border-bottom: 180px solid #41b883;
 }
-.Triangle--two
-{
+.Triangle--two {
   top: 30px;
   left: 35px;
   animation: goright 0.5s linear forwards 3.5s;
   border-left: 87.5px solid transparent;
   border-right: 87.5px solid transparent;
-  border-bottom: 150px solid #3B8070;
+  border-bottom: 150px solid #3b8070;
 }
-.Triangle--three
-{
+.Triangle--three {
   top: 60px;
   left: 35px;
   animation: goright 0.5s linear forwards 3.5s;
   border-left: 70px solid transparent;
   border-right: 70px solid transparent;
-  border-bottom: 120px solid #35495E;
+  border-bottom: 120px solid #35495e;
 }
-.Triangle--four
-{
+.Triangle--four {
   top: 120px;
   left: 70px;
   animation: godown 0.5s linear forwards 3s;
