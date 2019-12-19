420633299b84c06ce1af4cc0105cc893fb8d5537
Alex Gogl <gogl.alex@gmail.com>
2019-12-19 17:57:39 +0100

    upgrade everything to babel7

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,21 @@
-module.exports = {
-    env: {
-      test: {
-        presets: [
-          [
-            "@babel/env",
-            {
-              targets: {
-                node: 11
-              }
+function isBabelLoader(caller) {
+  return caller && caller.name === "babel-loader";
+}
+
+module.exports = function(api) {
+  if (api.env("test") && !api.caller(isBabelLoader)) {
+    return {
+      presets: [
+        [
+          "@babel/preset-env",
+          {
+            targets: {
+              node: "current"
             }
-          ]
+          }
         ]
-      }
-    }
-  };
+      ]
+    };
+  }
+  return {};
+};
